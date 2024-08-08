3c3a59674a3c04ad0515a2dd9eb3d3c5afa55c85
hiroto takao <biiiy080121@outlook.com>
2024-08-08 16:50:53 +0900

    ランキングの実装完了

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
 'use strict';
+import fs from "node:fs";
+import { setMaxIdleHTTPParsers } from "node:http";
+import readline from "node:readline";
+import xlsxPopulate from "xlsx-populate";
+const rs = fs.createReadStream("./popu-pref.csv");
+const rl = readline.createInterface({input: rs});
+const prefectureDataMap = new Map();
+rl.on("line", lineString=> {
+  const columns = lineString.split(",");
+  const year = parseInt(columns[0]);
+  const prefecture = columns[1];
+  const popu = parseInt(columns[3]);
+  if(year === 2016 || year === 2021) {
+    let value = null;
+    if (prefectureDataMap.has(prefecture)) {
+      value = prefectureDataMap.get(prefecture);
+    } else {
+      value = {
+        before: 0,
+        after: 0,
+        change: null
+      };
+    }
+    if (year === 2016) {
+      value.before = popu;
+    }
+    if (year === 2021) {
+      value.after = popu;
+    }
+    prefectureDataMap.set(prefecture, value);
+  }
+});
+
+rl.on("close", () => {
+  for(const [key, value] of prefectureDataMap) {
+    value.change = value.after / value.before;
+    value.difference = value.after - value.before;
+  }
+  const DescendingPercentage = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+    return pair2[1].change - pair1[1].change;
+  });
+
+  const rankingStrings = DescendingPercentage.map(([key, value]) => {
+    return `${key}: ${value.before}=>${value.after} 変化率：${value.change}`;
+  });
+  console.log(rankingStrings);
+});
